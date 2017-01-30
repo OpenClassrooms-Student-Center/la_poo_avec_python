e4a16094da3e8024a007975369245dc4c22160fd
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-01-30 16:45:05 +0100

    Add Position class and store coordinates in radians as properties

--- model.py
+++ model.py
@@ -1,14 +1,36 @@
 import json
+import math
 
 class Agent:
     
-    def __init__(self, **agent_attributes):
+    def __init__(self, position, **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.latitude_degrees = latitude_degrees
+        self.longitude_degrees = longitude_degrees
+
+    @property
+    def longitude(self):
+        # Longitude in radians
+        return self.longitude_degrees * math.pi / 180
+
+    @property
+    def latitude(self):
+        # Latitude in radians
+        return self.latitude_degrees * math.pi / 180
+
+
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(**agent_attributes)
-        print(agent.agreeableness)
+        latitude = agent_attributes.pop("latitude")
+        longitude = agent_attributes.pop("longitude")
+        position = Position(latitude, longitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.longitude)
+        print(agent.position.latitude)
 
 main()
