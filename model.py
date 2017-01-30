7d7640b7fb4d3341284ecf0f73164bd923ade777
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-01-30 13:17:02 +0100

    class attributes

--- model.py
+++ model.py
@@ -1,6 +1,14 @@
-class Agent:
-    pass
+import json
 
-first_agent = Agent()
-second_agent = Agent()
-print(first_agent, second_agent)
+class Agent:
+    
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
+
+main()
