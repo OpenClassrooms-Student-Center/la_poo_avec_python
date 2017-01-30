73fef383fc5b7bab660bb949a530a3dfb56eedfc
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-01-30 19:05:41 +0100

    Properties

--- model.py
+++ model.py
@@ -23,31 +23,6 @@ class Position:
         # Latitude in radians
         return self.latitude_degrees * math.pi / 180
 
-class Zone:
- 
-    ZONES = []
-    MIN_LONGITUDE_DEGREES = -180
-    MAX_LONGITUDE_DEGREES = 180
-    MIN_LATITUDE_DEGREES = -90
-    MAX_LATITUDE_DEGREES = 90 
-    WIDTH_DEGREES = 1 # degrees of longitude
-    HEIGHT_DEGREES = 1 # degrees of latitude
-
-    def __init__(self, corner1, corner2):
-        self.corner1 = corner1
-        self.corner2 = corner2
-        self.inhabitants = 0
-    
-    @classmethod
-    def initialize_zones(cls):
-        for latitude in range (cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
-            for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
-                bottom_left_corner = Position(longitude, latitude)
-                top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
-                zone = Zone(bottom_left_corner, top_right_corner)
-                cls.ZONES.append(zone)
-
-
 
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
@@ -55,6 +30,6 @@ def main():
         longitude = agent_attributes.pop("longitude")
         position = Position(latitude, longitude)
         agent = Agent(position, **agent_attributes)
-        Zone.initialize_zones()
+        print(agent.position.longitude)
+        print(agent.position.latitude)
 
-main()
