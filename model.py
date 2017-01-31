5db670024404410f7218517baecbc06682625750
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-01-31 19:15:55 +0100

    model finished

--- model.py
+++ model.py
@@ -32,6 +32,7 @@ class Zone:
     MAX_LATITUDE_DEGREES = 90 
     WIDTH_DEGREES = 1 # degrees of longitude
     HEIGHT_DEGREES = 1 # degrees of latitude
+    EARTH_RADIUS_KILOMETERS = 6371
 
     def __init__(self, corner1, corner2):
         self.corner1 = corner1
@@ -42,8 +43,33 @@ class Zone:
     def population(self):
         return len(self.inhabitants)
 
+    @property
+    def width(self):
+        return abs(self.corner1.longitude - self.corner2.longitude) * self.EARTH_RADIUS_KILOMETERS
+
+    @property
+    def height(self):
+        return abs(self.corner1.latitude - self.corner2.latitude) * self.EARTH_RADIUS_KILOMETERS
+
+    @property
+    def area(self):
+        """Compute the zone area, in square kilometers"""
+        return self.height * self.width
+
+    def population_density(self):
+        """Population density of the zone, (people/km²)"""
+        # Note that this will crash with a ZeroDivisionError if the zone has 0
+        # area, but it should really not happen
+        return self.population / self.area
+
+
     def add_inhabitant(self, inhabitant):
         self.inhabitants.append(inhabitant)
+
+    def average_agreeableness(self):
+        if not self.inhabitants:
+            return 0
+        return sum([inhabitant.agreeableness for inhabitant in self.inhabitants]) / self.population
 
     def contains(self, position):
         return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
@@ -53,6 +79,10 @@ class Zone:
 
     @classmethod
     def find_zone_that_contains(cls, position):
+        if not cls.ZONES:
+            # Initialize zones automatically if necessary
+            cls._initialize_zones()
+
         # Compute the index in the ZONES array that contains the given position
         longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES)/ cls.WIDTH_DEGREES)
         latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES)/ cls.HEIGHT_DEGREES)
@@ -66,7 +96,7 @@ class Zone:
         return zone
 
     @classmethod
-    def initialize_zones(cls):
+    def _initialize_zones(cls):
         # Note that this method is "private": we prefix the method name with "_".
         cls.ZONES = []
         for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
@@ -85,9 +115,9 @@ def main():
         longitude = agent_attributes.pop("longitude")
         position = Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-        Zone.initialize_zones()
         zone = Zone.find_zone_that_contains(position)
         zone.add_inhabitant(agent)
-        print(zone.population)
+        print(zone.average_agreeableness())
 
 main()
+0
