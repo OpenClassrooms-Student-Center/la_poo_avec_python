047d466bc00fc356cb48411230ae7a7da6a0c3ee
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-01-31 15:35:31 +0100

    CoffeeMachine to understand Encapsulation

--- model.py
+++ model.py
@@ -23,13 +23,75 @@ class Position:
         # Latitude in radians
         return self.latitude_degrees * math.pi / 180
 
+class Zone:
+
+    ZONES = []
+    MIN_LONGITUDE_DEGREES = -180
+    MAX_LONGITUDE_DEGREES = 180
+    MIN_LATITUDE_DEGREES = -90
+    MAX_LATITUDE_DEGREES = 90 
+    WIDTH_DEGREES = 1 # degrees of longitude
+    HEIGHT_DEGREES = 1 # degrees of latitude
+
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.inhabitants = []
+
+    @property
+    def population(self):
+        """Number of inhabitants in the zone"""
+        return len(self.inhabitants)
+
+    def add_inhabitant(self, inhabitant):
+        self.inhabitants.append(inhabitant)
+
+    def contains(self, position):
+        return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+            position.longitude < max(self.corner1.longitude, self.corner2.longitude) and \
+            position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+            position.latitude < max(self.corner1.latitude, self.corner2.latitude)
+
+    @classmethod
+    def find_zone_that_contains(cls, position):
+        if not cls.ZONES:
+            # Initialize zones automatically if necessary
+            cls._initialize_zones()
+
+        # Compute the index in the ZONES array that contains the given position
+        longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES)/ cls.WIDTH_DEGREES)
+        latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES)/ cls.HEIGHT_DEGREES)
+        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES) # 180-(-180) / 1
+        zone_index = latitude_index * longitude_bins + longitude_index
+
+        # Just checking that the index is correct
+        zone = cls.ZONES[zone_index]
+        assert zone.contains(position)
+
+        return zone
+
+    @classmethod
+    def _initialize_zones(cls):
+        # Note that this method is "private": we prefix the method name with "_".
+        cls.ZONES = []
+        for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
+            for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
+                bottom_left_corner = Position(longitude, latitude)
+                top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
+                zone = Zone(bottom_left_corner, top_right_corner)
+                cls.ZONES.append(zone)
+
+
+
 
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
         latitude = agent_attributes.pop("latitude")
         longitude = agent_attributes.pop("longitude")
-        position = Position(latitude, longitude)
+        position = Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-        print(agent.position.longitude)
-        print(agent.position.latitude)
+        zone = Zone.find_zone_that_contains(position)
+        zone.add_inhabitant(agent)
+        # print(zone.population)
 
+main()
